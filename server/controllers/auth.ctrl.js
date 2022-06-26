85ab34373c1de551d01ecf29f63eddfe07cfd1db
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-06-27 00:28:47 +0300

    created validationMiddleware

--- server/controllers/auth.ctrl.js
+++ server/controllers/auth.ctrl.js
@@ -1,68 +1,47 @@
 const { Router } = require("express");
-const Joi = require("joi");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { v4: uuidv4 } = require("uuid");
 const { getUserByUsernameOrEmail, addUser } = require("../bl");
+const {
+  validateRegisterMiddleware,
+  validateLoginMiddleware,
+} = require("../middlewares");
+
 require("dotenv").config();
 const auth = Router();
 const saltRounds = 10;
 
-const registerSchema = Joi.object({
-  email: Joi.string().email({ minDomainSegments: 2 }).required(),
-  username: Joi.string().min(3).max(40).required(),
-  password: Joi.string().min(6).max(40).required(),
-  firstName: Joi.string().min(3).max(40).required(),
-  lastName: Joi.string().min(6).max(40).required(),
-  confirmPassword: Joi.ref("password"),
-});
+auth.post("/register", validateRegisterMiddleware, async (req, res) => {
+  const hash = await bcrypt.hash(req.body.password, saltRounds);
 
-auth.post("/register", async (req, res) => {
-  const { error, value } = registerSchema.validate(req.body);
-  if (error) {
-    const errorMessage =
-      error.message == '"confirmPassword" must be [ref:password]'
-        ? "Confirm Password and password must match"
-        : error.message;
-    return res.status(400).send(errorMessage);
-  }
-
-  const hash = await bcrypt.hash(value.password, saltRounds);
-
-  delete value["confirmPassword"];
   try {
-    const newUser = await addUser({ ...value, password: hash, id: uuidv4() });
-console.log(newUser);
+    const newUser = await addUser({
+      ...req.body,
+      password: hash,
+      id: uuidv4(),
+    });
+
     if (newUser) {
       const token = jwt.sign(
-        { username: value.username, id: newUser.id },
+        { username: req.body.username, id: newUser.id },
         process.env.SECRET_KEY
-      ); res.status(201).json({ token, id: newUser.id });
+      );
+      res.status(201).json({ token, id: newUser.id });
     }
-   
   } catch (error) {
     if (error.message.split(" ")[0] === "Duplicate") {
       return res
         .status(409)
         .send("Username already exist! Please try a different one!");
     }
-    console.log(error.message)
+    console.log(error.message);
     return res.sendStatus(500);
   }
 });
 
-const loginSchema = Joi.object({
-  username: Joi.string(),
-  email: Joi.string().email({ minDomainSegments: 2 }),
-  password: Joi.string().required(),
-}).xor("username", "email");
-
-auth.post("/login", async (req, res) => {
-  const { error } = loginSchema.validate(req.body);
-  if (error) return res.status(400).send(error.message);
-
+auth.post("/login", validateLoginMiddleware, async (req, res) => {
   const { username, email, password } = req.body;
-
   try {
     const user = await getUserByUsernameOrEmail({ username, email });
 
---
+++ server/middlewares/validationMiddlewares.js
@@ -0,0 +1,60 @@
+const Joi = require("joi");
+
+function validateRegisterMiddleware(req, res, next) {
+  const registerSchema = Joi.object({
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    username: Joi.string().min(3).max(40).required(),
+    password: Joi.string().min(6).max(40).required(),
+    firstName: Joi.string().min(3).max(40).required(),
+    lastName: Joi.string().min(6).max(40).required(),
+    confirmPassword: Joi.string()
+      .required()
+      .valid(Joi.ref("password"))
+      .error(new Error("Confirm Password and password must match")),
+  });
+
+  const { error, value } = registerSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.message);
+  }
+  delete value["confirmPassword"];
+  req.body = value;
+  next();
+}
+
+function validateLoginMiddleware(req, res, next) {
+  const loginSchema = Joi.object({
+    username: Joi.string(),
+    email: Joi.string().email({ minDomainSegments: 2 }),
+    password: Joi.string().required(),
+  }).xor("username", "email");
+
+  const { error } = loginSchema.validate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  next();
+}
+
+function validatedVacationMiddleware(req, res, next) {
+  const vacationSchema = Joi.object({
+    desription: Joi.string().min(10).max(36000),
+    destination: Joi.string().min(3).max(60),
+    picture: Joi.string().min(4).max(100).optional().allow(null),
+    startDate: Joi.date(),
+    endDate: Joi.date(),
+    price: Joi.number().precision(2),
+  }).alter({
+    POST: (schema) => schema.required(),
+    PUT: (schema) => schema.min(1).error(new Error("You must change at least one thing")),
+  });
+
+  const { error, value } = vacationSchema.tailor(req.method).validate(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  next();
+}
+module.exports = {
+  validateLoginMiddleware,
+  validateRegisterMiddleware,
+  validatedVacationMiddleware,
+};
