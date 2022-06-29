46347c33130e3b490207743bfb834cd943c82ec5
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-06-30 02:28:58 +0300

    setup store

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,12 +1,29 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import authSlice from '../features/auth/authSlice';
+import { apiSlice } from './api/apiSlice';
+
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+   auth:authSlice
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools:true
 });
 
+// const authMiddleware = (store) => (next) => (action) => {
+//   if (authActions.login.match(action)) {
+//     // Note: localStorage expects a string
+//     localStorage.setItem("isAuthenticated", "true");
+//   } else if (authActions.logout.match(action)) {
+//     localStorage.setItem("isAuthenticated", "false");
+//   }
+//   return next(action);
+// };
+setupListeners(store.dispatch)
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
