13c71a860ea118ee9743022f3ccd356e9451600a
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-01 19:20:19 +0300

    refetch for user details on reload

--- client/src/features/auth/RequireAuth.tsx
+++ client/src/features/auth/RequireAuth.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
-import { Navigate, useLocation, Outlet } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { selectCurrentToken } from "./authSlice";
-import { store } from "../../app/store";
+import { selectCurrentToken, selectCurrentUserId } from "./authSlice";
+import usersVacationsApi from "../vacations/userVacationsApiSlice";
+import { useAppDispatch } from "../../app/hooks";
+import { authApiSlice } from "./authApiSlice";
 
 export function RequireAuth({ children }: { children: JSX.Element }) {
   let location = useLocation();
+  const dispatch = useAppDispatch();
   const token = useSelector(selectCurrentToken);
-
+  const userId = useSelector(selectCurrentUserId);
+  
+  if (!userId) dispatch(authApiSlice.endpoints.onRefresh.initiate(0));
   return token ? (
     children
   ) : (
