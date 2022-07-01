462c847618de70e411c1c7bbe8ce89ae6950bd2e
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-01 19:18:51 +0300

    dispatch to getAllVacations in Home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useAppDispatch } from "../app/hooks";
+import { store } from "../app/store";
+import { selectCurrentToken } from "../features/auth/authSlice";
+import usersVacationsApi, {
+  useGetAllVacationsQuery,
+} from "../features/vacations/userVacationsApiSlice";
+import { Vacations } from "../features/vacations/vacations.interface";
+import VacationsList from "../features/vacations/VacationsList";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  const dispatch = useAppDispatch();
+  const token = useSelector(selectCurrentToken)
+  if (token) dispatch(usersVacationsApi.endpoints.getAllVacations.initiate([]));
+
+  return <><VacationsList/></>;
 }
 
-export default Home
+export default Home;
