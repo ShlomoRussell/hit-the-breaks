b16c5f44bfc516d2b91a3dd5706b0ae90c5161c5
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-04 17:15:38 +0300

    added spinner

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,20 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useAppDispatch } from "../app/hooks";
 import { store } from "../app/store";
-import { selectCurrentToken } from "../features/auth/authSlice";
+
 import usersVacationsApi, {
   useGetAllVacationsQuery,
 } from "../features/vacations/userVacationsApiSlice";
 import { Vacations } from "../features/vacations/vacations.interface";
 import VacationsList from "../features/vacations/VacationsList";
+import Header from "./Header";
 
 function Home() {
-  const dispatch = useAppDispatch();
-  const token = useSelector(selectCurrentToken)
-  if (token) dispatch(usersVacationsApi.endpoints.getAllVacations.initiate([]));
-
-  return <><VacationsList/></>;
+  const { isLoading } = useGetAllVacationsQuery([]);
+  return (
+    <div style={{ backgroundColor: "#f0f2f5" }}>
+      <Header />
+      {isLoading ? (
+        <div className="position-relative vh-100">
+          <Spinner
+            animation="border"
+            className="position-absolute top-50 start-50 translate-middle"
+            role="status"
+            variant="info"
+          >
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <VacationsList />
+      )}
+    </div>
+  );
 }
 
 export default Home;
