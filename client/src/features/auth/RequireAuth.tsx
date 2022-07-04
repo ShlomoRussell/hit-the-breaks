4afd722c62c3deddee6f493914cdf9ae77b8063c
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-04 17:17:45 +0300

    put dispatch into useEffect

--- client/src/features/auth/RequireAuth.tsx
+++ client/src/features/auth/RequireAuth.tsx
@@ -4,14 +4,21 @@ import { selectCurrentToken, selectCurrentUserId } from "./authSlice";
 import usersVacationsApi from "../vacations/userVacationsApiSlice";
 import { useAppDispatch } from "../../app/hooks";
 import { authApiSlice } from "./authApiSlice";
+import { useEffect } from "react";
 
 export function RequireAuth({ children }: { children: JSX.Element }) {
   let location = useLocation();
   const dispatch = useAppDispatch();
   const token = useSelector(selectCurrentToken);
   const userId = useSelector(selectCurrentUserId);
+
   
-  if (!userId) dispatch(authApiSlice.endpoints.onRefresh.initiate(0));
+  useEffect(() => {
+    if (!userId) {
+      dispatch(authApiSlice.endpoints.onRefresh.initiate(0));
+    }
+  }, []);
+
   return token ? (
     children
   ) : (
