9abff058d90c03349b102ba7bf6063e710e59a4e
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-08 09:19:59 +0300

    route edit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,22 @@ import Login from "./features/auth/Login";
 import Register from "./features/auth/Register";
 import Home from "./components/Home";
 import { RequireAuth } from "./features/auth/RequireAuth";
-import { store } from "./app/store";
+import EditVacation from "./features/vacations/EditVacation";
 
 function App() {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/edit/:vacationid" element={<EditVacation />} />
       <Route
-        path="/"
+        path="/*"
         element={
           <RequireAuth>
             <Home />
           </RequireAuth>
         }
-      />
+      ></Route>
     </Routes>
   );
 }
