6e0b5c16381e26170f4b6f4f0593beec665fc6bf
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-08 09:21:39 +0300

    Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { selectIsAdmin } from "../features/auth/authSlice";
+import MenuDropdown from "./MenuDropdown";
 
-function Header() {
+function Header(): JSX.Element {
+   const isAdmin = useSelector(selectIsAdmin);
   return (
-    <Navbar sticky='top' bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar sticky="top" style={{ backgroundColor: "#1a63a1" }}>
+      <Container fluid>
+        <Navbar.Brand to={"/"} as={Link}>
+          <div>
+            <Image
+              src="/logo7.png"
+              roundedCircle
+              style={{
+                objectFit: "scale-down",
+                maxWidth: "100%",
+                height: "auto",
+              }}
+            />
+          </div>
+        </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link style={{ color: "whitesmoke" }} to={"/"} as={Link}>
+            Home
+          </Nav.Link>
+          <Nav.Link style={{ color: "whitesmoke" }} to={""} as={Link}>
+            Vacations
+          </Nav.Link>
+          {isAdmin && (
+            <Nav.Link style={{ color: "whitesmoke" }} to={""} as={Link}>
+              Reports
+            </Nav.Link>
+          )}
+        </Nav>
+        <Nav>
+          <Nav.Link as={MenuDropdown} title="Menu" />
         </Nav>
       </Container>
     </Navbar>
   );
 }
 
-export default Header
+export default Header;
