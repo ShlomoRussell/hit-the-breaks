e0eae83fa52f9cfbfc0ea08aadf60566d0e717c0
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-12 16:51:37 +0300

    move edit and add links to MenuDropdown

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,16 +1,13 @@
 import { Container, Image, Nav, Navbar } from "react-bootstrap";
-import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { selectIsAdmin } from "../features/auth/authSlice";
 import MenuDropdown from "./MenuDropdown";
 
 function Header(): JSX.Element {
-   const isAdmin = useSelector(selectIsAdmin);
   return (
     <Navbar sticky="top" style={{ backgroundColor: "#1a63a1" }}>
       <Container fluid>
         <Navbar.Brand to={"/"} as={Link}>
-          <div style={{height:'4rem',width:"4rem"}}>
+          <div style={{ height: "4rem", width: "4rem" }}>
             <Image
               src="/logo.png"
               alt="logo"
@@ -24,17 +21,12 @@ function Header(): JSX.Element {
           </div>
         </Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link  style={{ color: "whitesmoke" }} to={"/"} as={Link}>
+          <Nav.Link style={{ color: "whitesmoke" }} to={"/"} as={Link}>
             Home
           </Nav.Link>
           <Nav.Link style={{ color: "whitesmoke" }} to={"/vacations"} as={Link}>
             Vacations
           </Nav.Link>
-          {isAdmin && (
-            <Nav.Link style={{ color: "whitesmoke" }} to={"/reports"} as={Link}>
-              Reports
-            </Nav.Link>
-          )}
         </Nav>
         <Nav>
           <Nav.Link as={MenuDropdown} title="Menu" />
--- client/src/components/MenuDropdown.tsx
+++ client/src/components/MenuDropdown.tsx
@@ -2,7 +2,9 @@ import { Dropdown, Nav } from "react-bootstrap";
 import { forwardRef } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import { useAppDispatch } from "../app/hooks";
-import { logOut } from "../features/auth/authSlice";
+import { logOut, selectIsAdmin } from "../features/auth/authSlice";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const CustomToggle = forwardRef(
   (
@@ -28,14 +30,25 @@ function MenuDropdown() {
   const handleLogOut = () => {
     dispatch(logOut(null));
   };
+     const isAdmin = useSelector(selectIsAdmin);
+
   return (
     <Dropdown drop="start" className="d-inline" autoClose="outside">
-      <Dropdown.Toggle  as={CustomToggle} id="dropdown-custom-components" />
+      <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components" />
 
       <Dropdown.Menu align="start">
         <Dropdown.Item eventKey="1">Profile</Dropdown.Item>
         <Dropdown.Item eventKey="2">My Vacations</Dropdown.Item>
-        <Dropdown.Item eventKey="3">Orange</Dropdown.Item>
+        {isAdmin && (
+          <>
+            <Dropdown.Item to={"/reports"} as={Link} eventKey="3">
+              Reports
+            </Dropdown.Item>
+            <Dropdown.Item to={"/vacations/add"} as={Link} eventKey="3">
+              Add Vacation
+            </Dropdown.Item>
+          </>
+        )}
         <Dropdown.Item eventKey="4" title="Log out" onClick={handleLogOut}>
           Log out
         </Dropdown.Item>
