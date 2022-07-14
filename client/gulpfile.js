3c87990aaee3c0a3635ffc5456a5a054eb949875
ShlomoRussell <93613042+ShlomoRussell@users.noreply.github.com>
2022-07-14 20:01:58 +0300

    edited gulpfile

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,15 +3,18 @@ var path = require("path");
 const { series, src, dest } = require("gulp");
 const del = require("del");
 
-const sourceDir = path.join(__dirname, "build");
+const sourceDir = path.posix.join(__dirname, "build");
 const destDir = path.join(__dirname, "../", "server", "public");
 
-function cleanTarget(cb) {
-  return del(`${sourceDir}/**`, { force: true });
+function cleanSource(cb) {
+  return del(`${sourceDir}/`, { force: true });
 }
 
 function copyToTarget(cb) {
   return src(`${sourceDir}/**`).pipe(dest(destDir));
 }
+function cleanTarget(cb) {
+  return del(`${destDir}/`, { force: true });
+}
 
-exports.default = series(cleanTarget, copyToTarget);
+exports.default = series(cleanTarget,copyToTarget, cleanSource);
